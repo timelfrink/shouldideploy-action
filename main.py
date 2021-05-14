4fc283e1f879967afe0c5abbc3e84f65ffff5fa1
Tim Elfrink <tim.elfrink@vantage-ai.com>
2021-05-14 11:30:50 +0200

    Create API call

---
+++ main.py
@@ -0,0 +1,9 @@
+import urllib.request
+import json
+import sys
+
+url = f"https://shouldideploy.today/api?tz={sys.argv[1]}"
+req = urllib.request.urlopen(url)
+
+data = json.loads(req.read().decode("utf-8"))
+print(data)
