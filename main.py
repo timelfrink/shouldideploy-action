124e03c205b79151ef6d3edd939dc29cee93d5b3
Tim Elfrink <tim.elfrink@vantage-ai.com>
2021-05-14 12:11:28 +0200

    add test

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ url = f"https://shouldideploy.today/api?tz={sys.argv[1]}"
 req = urllib.request.urlopen(url)
 
 data = json.loads(req.read().decode("utf-8"))
+print('test')
 print(data)
 print(f"::set-output name=deploy::{data['shouldideploy']}")
+print('test2')
